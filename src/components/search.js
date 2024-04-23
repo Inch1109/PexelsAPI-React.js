1079357aebefc41d4f15a8cc9047282955e1c0a4
Inch1109 <inchgum1109@gmail.com>
2024-04-24 00:00:13 +0800

    修改search的bug

--- src/components/search.js
+++ src/components/search.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
 
+export default function Search({
+  search = () => {},
+  setInput = () => {},
+  input = "",
+}) {
+  const inputHandler = (e) => {
+    setInput(e.target.value);
+  };
 
-export default function Search({search=()=>{},setInput=()=>{}} ) {
- 
-    const inputHandler = (e)=>{
-        setInput(e.target.value)
-    }
   return (
     <div className="search">
-      <input className="input" type="text" onChange={inputHandler}/>
-      <button onClick={search}><FaSearch /></button>
+      <input className="input" type="text" onChange={inputHandler} />
+      {/* 當input未輸入文字，點擊btn會跳出404 */}
+      <button onClick={search} disabled={input === "" ? true : false}>
+        <FaSearch />
+      </button>
     </div>
   );
 }
--- src/page/homePage.js
+++ src/page/homePage.js
@@ -7,7 +7,7 @@ export default function HomePage() {
   let [input, setInput] = useState("");
   let [data, setData] = useState([]); //取得圖片資訊
   let [page, setPage] = useState(1);
-  let [currentSearch, setCurrentSearch] = useState("")
+  let [currentSearch, setCurrentSearch] = useState("");
   const auth = "gkGMM2II5X8GsEqZ5zyegDSMBcdOVN1byJ4Vdc9K3qEdT4ap3uzasdR9";
   const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
   let searchURL = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=15`;
@@ -17,24 +17,27 @@ export default function HomePage() {
     let result = await axios.get(url, {
       headers: { Authorization: auth },
     });
+   
     // console.log(result);
     setData(result.data.photos);
-    setCurrentSearch(input)
+    setCurrentSearch(input);
   };
- // 點選more會往下加載圖片 
+  // 點選more會往下加載圖片
   /* 遇到問題1. 圖片加載第2頁，page=${page}還是1，但react狀態有改成2。
-    是因為“閉包“，抓到最外面的page*/ 
+    是因為“閉包“，抓到最外面的page*/
   /* 問題2. 如果在input打字，沒有按收尋，但是點選more會出現收尋的圖片。
      所以要把input狀態和搜尋狀態分開 */
   const morePicture = async () => {
     let newURL;
     setPage(page + 1);
     if (currentSearch === "") {
-      newURL = `https://api.pexels.com/v1/curated?page=${page+1}&per_page=15`;
+      newURL = `https://api.pexels.com/v1/curated?page=${page + 1}&per_page=15`;
     } else {
-      newURL = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=${page+1}`;
+      newURL = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=${
+        page + 1
+      }`;
     }
-    console.log(newURL)
+    console.log(newURL);
     let result = await axios.get(newURL, {
       headers: { Authorization: auth },
     });
@@ -53,6 +56,8 @@ export default function HomePage() {
           search(searchURL);
         }}
         setInput={setInput}
+        input={input}
+        initialURL={initialURL}
       />
       <div className="pictures">
         {/* 因為data預設是[]，所以沒有資料的時候不會顯示東西。
